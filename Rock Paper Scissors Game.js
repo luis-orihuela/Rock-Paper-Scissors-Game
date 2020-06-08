23b33fb701715ab629beec4dfef833752073a1a5
luis-orihuela <53580523+luis-orihuela@users.noreply.github.com>
2020-06-07 21:48:21 -0700

    Update Rock Paper Scissors Game.js

--- Rock Paper Scissors Game.js
+++ Rock Paper Scissors Game.js
@@ -1,45 +1,60 @@
-const getUserChoice = userInput => { 
+const getUserChoice = userInput => {
   userInput = userInput.toLowerCase();
-  return userInput;
-};
- const getComputerChoice = randomNumber => {
-   randomNumber = Math.floor(Math.random() * 3);
-switch (randomNumber) {
-  case 0 :
-  return 'paper';
-  break;
-  case 1 :
-  return 'scissors';
-  break;
-  case 2 :
-  return 'rock';
-  break;
-  }
-};
-const determineWinner = (userChoice,computerChoice) => {
-  if (userChoice === computerChoice){
-    return 'It is a tie!';
-  } else if (userChoice === 'rock' && computerChoice == 'paper') {
-     return 'Computer wins!';
-  } else if (userChoice === 'scissors' && computerChoice == 'rock') {
-    return 'Computer wins!';
-  } else if (userChoice === 'paper' && computerChoice == 'scissors') {
-   return 'Computer wins!';
-    } else {
-   return 'User wins!';
-   }
-};
-const playGame = () => {
-  const userChoice = getUserChoice('');
-  const computerChoice = getComputerChoice();
-  if (userChoice === 'rock' || userChoice === 'paper' || userChoice === 'scissors') {
-  console.log('User chose: ' + userChoice);
-  console.log('Computer chose: ' + computerChoice);
-  console.log(determineWinner(userChoice,computerChoice));
-  }  else {
-    console.log('Error. User entered: ' + '[' + userChoice + ']. ' + 'Please enter rock, paper, or scissors.');
-    return;
-  }
-}
-playGame();
+    if (userInput === 'rock' || userInput === 'paper'  || userInput === 'scissors'|| userInput === 'bomb') {
+      return userInput;
+    }else{
+      console.log('Error!');
+    }
+    }
 
+function getComputerChoice() {
+  computerInput = Math.floor(Math.random() * 3);
+  switch (computerInput){
+    case 0:
+      return 'rock';
+      break;
+    case 1:
+      return 'paper';
+      break;
+    case 2:
+      return 'scissors';
+      break;
+  }
+  return computerInput;
+} 
+
+function determineWinner(userChoice,computerChoice) {
+  if (userChoice === computerChoice) {
+    return 'It\'s a tie!';
+  } else if (userChoice === 'rock') {
+    if (computerChoice === 'paper') {
+      return 'Computer wins!';
+    } else {
+      return 'You win!';
+    }
+  } else if (userChoice === 'paper'){
+    if (computerChoice === 'scissors') {
+      return 'Computer wins!';
+    }else {
+      return 'You win!';
+    }
+  } else if (userChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+      return 'Computer wins!';
+    } else {
+      return 'You win!';
+    }
+  } else if (userChoice === 'bomb') {
+    return 'You win!';
+  }
+};
+
+function playGame() {
+  let userChoice = getUserChoice('paper');
+  let computerChoice = getComputerChoice()
+  console.log(`you threw ${userChoice}`);
+  console.log(`computer threw ${computerChoice}`);
+  console.log(determineWinner(userChoice,computerChoice));
+};
+
+playGame();
